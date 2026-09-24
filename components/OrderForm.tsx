115e1719a917b371c435f277218647ffd181d2aa
t-rik <ziane.aymn@gmail.com>
2026-09-24 18:50:08 +0100

    fix(ui): remove phone format hint under phone input

--- components/OrderForm.tsx
+++ components/OrderForm.tsx
@@ -337,7 +337,6 @@ export default function OrderForm({ product, onBundleChange }: OrderFormProps) {
                 />
                 <Phone className="w-5 h-5 text-gray-400 absolute left-3 top-1/2 -translate-y-1/2" />
               </div>
-              <p className="text-[11px] text-gray-400 mt-1">{t("phone_hint")}</p>
               {errors.phone && (
                 <p className="text-xs text-red-600 font-medium mt-1">{errors.phone}</p>
               )}
